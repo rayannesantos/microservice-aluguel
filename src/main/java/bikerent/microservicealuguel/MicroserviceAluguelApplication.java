951e90e3a99f9d2942a93fbef05df0e156b7fe78
rayannesantos <santosrayanne.s@gmail.com>
2023-11-12 20:10:31 -0300

    feat: Add Java template

---
+++ src/main/java/bikerent/microservicealuguel/MicroserviceAluguelApplication.java
@@ -0,0 +1,13 @@
+package bikerent.microservicealuguel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAluguelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAluguelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bikerent/microservicealuguel/MicroserviceAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package bikerent.microservicealuguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
