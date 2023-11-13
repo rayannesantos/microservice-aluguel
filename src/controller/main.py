2df33599a05f2f530d83640a9cf5de274475c699
rayannesantos <santosrayanne.s@gmail.com>
2023-11-13 02:50:24 -0300

    add cadastrar_ciclista

--- src/controller/main.py
+++ src/controller/main.py
@@ -1,10 +1,41 @@
+import requests, os, sys
 from flask import Flask
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest
 
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, project_root)
+
+from service.CiclistaService import cadastrar_ciclista
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello World!'
+
+
+@app.route('/ciclista', methods=['POST'])
+def cadastrar_ciclista_route():
+        data = request.get_json()
+        nome =  data.get("ciclista", {}).get("nome")
+        nascimento = data.get("ciclista", {}).get("nascimento")
+        cpf = data.get("ciclista", {}).get("cpf")
+        passaporteNumero = data.get("ciclista", {}).get("passaporte", {}).get("numero")
+        passaporteValidade =  data.get("ciclista", {}).get("passaporte", {}).get("validade")
+        passaportePais =  data.get("ciclista", {}).get("passaporte", {}).get("pais")
+        nacionalidade = data.get("ciclista", {}).get("nacionalidade")
+        
+        email = data.get("ciclista", {}).get("email")
+        urlFotoDocumento = data.get("ciclista", {}).get("urlFotoDocumento")
+        senha = data.get("ciclista", {}).get("senha")
+        nomeTitular = data.get("meioDePagamento", {}).get("nomeTitular")
+        numeroCartao = data.get("meioDePagamento", {}).get("numero")
+        validadeCartao = data.get("meioDePagamento", {}).get("validade")
+        cvv = data.get("meioDePagamento", {}).get("cvv")
+        
+    
+        response = cadastrar_ciclista(nome, nascimento,cpf, passaporteNumero, passaporteValidade, passaportePais, nacionalidade,
+                                      email,urlFotoDocumento,senha, nomeTitular, numeroCartao, validadeCartao, cvv)
+
+        return response
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/service/CiclistaService.py
@@ -0,0 +1,55 @@
+import random
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest
+from unittest.mock import Mock
+
+app = Flask(__name__)
+
+
+def cadastrar_ciclista(nome, nascimento, cpf, passaporteNumero, passaporteValidade, passaportePais, nacionalidade, email, 
+                       urlFotoDocumento,senha, nomeTitular, numeroCartao, validadeCartao, cvv ):
+    
+    # TODO validarEmail
+    response_mock = Mock()
+    response_mock.status_code = "Dados cadastrados", 200
+
+    validacao = True
+    
+    if not validacao:
+        response_mock.status_code = 422
+        response_mock.json.return_value = [
+            {
+                "codigo": 422,
+                "mensagem": "Dados inválidos"
+            }
+        ]
+        return response_mock.json()
+    
+    response_mock.json.return_value = {
+        "ciclista": {
+            # TODO validação para apenas numeros que não foram usados
+            "id": random.randint(1, 1000),
+            "nome": nome,
+            "nascimento": nascimento,
+            "cpf": cpf,
+            "passaporte": {
+                "numero": passaporteNumero,
+                "validade": passaporteValidade,
+                "pais": passaportePais
+            },
+            "nacionalidade": nacionalidade,
+            "email": email,
+            "urlFotoDocumento": urlFotoDocumento,
+            "senha": senha
+        },
+        "meioDePagamento": {
+            "nomeTitular": nomeTitular,
+            "numero": numeroCartao,
+            "validade": validadeCartao,
+            "cvv": cvv
+        }
+    }
+
+    return response_mock.json()
+
+
