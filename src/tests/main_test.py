adf1a5229f9782fe4190de128e2e2763e4eded5a
Juliana Marques <julianamarquesdsc@gmail.com>
2023-11-14 07:22:14 -0300

    add tests

--- src/tests/main_test.py
+++ src/tests/main_test.py
@@ -18,6 +18,39 @@ class TestMain(unittest.TestCase):
             self.assertEqual(response.status_code, mock_listar_ciclista.status_code)
 
     @patch('controller.main.Mock')
+    def test_cadastrar_ciclista_route(self, mock_cadastrar_ciclista):
+
+        mock_cadastrar_ciclista.status_code = 200
+
+        data = {
+            "ciclista": {
+                "id_ciclista": 3,
+                "nome": "nome",
+                "nascimento": "nascimento",
+                "cpf": "cpf",
+                "passaporte": {
+                    "numero": "passaporte_numero",
+                    "validade": "passaporte_validade",
+                    "pais": "passaporte_pais"
+                },
+                "nacionalidade": "nacionalidade",
+                "email": "email",
+                "url_foto_documento": "url_foto_documento",
+                "senha": "senha"
+            },
+            "meio_de_pagamento": {
+                "nome_titular": "nome_titular",
+                "numero": "numero_cartao",
+                "validade": "validade_cartao",
+                "cvv": "cvv"
+            }
+        }
+
+        with app.test_client() as client:
+            response = client.post('/ciclista', json=data)
+            self.assertEqual(response.status_code, mock_cadastrar_ciclista.status_code)
+
+    @patch('controller.main.Mock')
     def test_ativar_ciclista_route(self, mock_ativar_ciclista):
 
         mock_ativar_ciclista.status_code = 200
@@ -55,6 +88,28 @@ class TestMain(unittest.TestCase):
         with app.test_client() as client:
             response = client.post('/funcionario', headers={"Content-Type": "application/json"}, json=data)
             self.assertEqual(response.status_code, mock_cadastrar_funcionario.status_code)
+
+
+    @patch('controller.main.Mock')
+    def test_editar_funcionario_route(self, mock_editar_funcionario):
+
+        mock_editar_funcionario.status_code = 200
+
+        data = {
+            "id_funcionario": 2,
+            "matricula": "teste",
+            "senha": "teste",
+            "confirmacaoSenha": "teste",
+            "email": "teste@example.com",
+            "nome": "teste",
+            "idade": 19,
+            "funcao": "REPARADOR",
+            "cpf": "teste"
+        }
+
+        with app.test_client() as client:
+            response = client.put('/funcionario/2', headers={"Content-Type": "application/json"}, json=data)
+            self.assertEqual(response.status_code, mock_editar_funcionario.status_code) 
 
 if __name__ == '__main__':
     unittest.main()
