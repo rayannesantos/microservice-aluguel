197363b7471fe71b553de52113bcb1a215def224
rayannesantos <santosrayanne.s@gmail.com>
2023-12-01 00:50:08 -0300

    update main.py

--- src/controller/main.py
+++ src/controller/main.py
@@ -1,6 +1,6 @@
 import os, sys
-from flask import Flask,request
 from unittest.mock import Mock
+from flask import Blueprint, jsonify,request
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
@@ -33,53 +33,62 @@ def hello_world():
 
 # FUNCIONARIOS 
 
-@app.route('/funcionarios', methods=['GET'])
-def listar_funcionarios_route():
-    return listar_funcionarios()
+# @app.route('/funcionarios', methods=['GET'])
+# def listar_funcionarios_route():
+#     return listar_funcionarios()
 
-@app.route('/funcionarios/<int:id_funcionario>', methods=['GET'])
-def listar_funcionarios_id_route(id_funcionario):
-    return listar_funcionario_id(id_funcionario)
-
-
-@app.route('/funcionario', methods=['POST'])
-def cadastrar_funcionario_route():
-        data = request.json
-        senha = data.get("senha")
-        confirmacao_senha = data.get("confirmacao_senha")
-        email =  data.get("email")
-        nome = data.get("nome")
-        idade = data.get("idade")
-        funcao = data.get("funcao")
-        cpf = data.get("cpf")
-
-        response = cadastrar_funcionario(senha, confirmacao_senha, email, nome, idade, funcao, cpf)
-        return response
+# @app.route('/funcionarios/<int:id_funcionario>', methods=['GET'])
+# def listar_funcionarios_id_route(id_funcionario):
+#     return listar_funcionario_id(id_funcionario)
 
 
-@app.route('/funcionario/<int:id_funcionario>', methods=['PUT'])
-def editar_funcionario_route(id_funcionario):
-        data = request.json
-        senha = data.get("senha")
-        confirmacao_senha = data.get("confirmacao_senha")
-        email =  data.get("email")
-        nome = data.get("nome")
-        idade = data.get("idade")
-        funcao = data.get("funcao")
-        cpf = data.get("cpf")
+# @app.route('/funcionario', methods=['POST'])
+# def cadastrar_funcionario_route():
+#         data = request.json
+#         senha = data.get("senha")
+#         confirmacao_senha = data.get("confirmacao_senha")
+#         email =  data.get("email")
+#         nome = data.get("nome")
+#         idade = data.get("idade")
+#         funcao = data.get("funcao")
+#         cpf = data.get("cpf")
 
-        response = editar_funcionario(id_funcionario, senha, confirmacao_senha, email, nome, idade, funcao,cpf)
-        return response
+#         response = cadastrar_funcionario(senha, confirmacao_senha, email, nome, idade, funcao, cpf)
+#         return response
+
+
+# @app.route('/funcionario/<int:id_funcionario>', methods=['PUT'])
+# def editar_funcionario_route(id_funcionario):
+#         data = request.json
+#         senha = data.get("senha")
+#         confirmacao_senha = data.get("confirmacao_senha")
+#         email =  data.get("email")
+#         nome = data.get("nome")
+#         idade = data.get("idade")
+#         funcao = data.get("funcao")
+#         cpf = data.get("cpf")
+
+#         response = editar_funcionario(id_funcionario, senha, confirmacao_senha, email, nome, idade, funcao,cpf)
+#         return response
     
 
-@app.route('/funcionario/<int:id_funcionario>', methods=['DELETE'])
-def remover_funcionario_route(id_funcionario):
-        response = remover_funcionario(id_funcionario)
-        return response
+# @app.route('/funcionario/<int:id_funcionario>', methods=['DELETE'])
+# def remover_funcionario_route(id_funcionario):
+#         response = remover_funcionario(id_funcionario)
+#         return response
+
+# LISTA TODOS OS CICLISTAS
+@ciclista_app.route('/', methods=['GET'])
+def listar_todos_ciclistas_route():
+    try:
+        ciclistas = ciclista_service.listar_todos()
+        return jsonify({"ciclistas": ciclistas})
+    except Exception:
+        return jsonify({"error": "Not Found"}), 404
     
 
 if __name__ == '__main__':
-    app.register_blueprint(ciclista_app, url_prefix='/ciclista')
+    # app.register_blueprint(ciclista_app, url_prefix='/ciclista')
     app.register_blueprint(aluguel_app, url_prefix='/aluguel')
     app.register_blueprint(devolucao_app, url_prefix='/devolucao')
     app.run(port=int(os.environ.get("PORT", 8080)),host='0.0.0.0',debug=True)
