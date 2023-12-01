f7a78021ddcbe8e70eda517c099b06b0a813eaeb
rayannesantos <santosrayanne.s@gmail.com>
2023-12-01 00:57:19 -0300

    update main.py

--- src/controller/main.py
+++ src/controller/main.py
@@ -1,6 +1,6 @@
 import os, sys
 from unittest.mock import Mock
-from flask import Blueprint, jsonify,request
+from flask import Blueprint, jsonify,request,Flask
 from service.CiclistaService import CiclistaService
 
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
